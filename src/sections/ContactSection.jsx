46d514c921db206ea7d11bfcc68de28844de141f
Nikhil sai ankilla <109269825+nikhilsaiankilla@users.noreply.github.com>
2024-05-05 14:41:46 +0530

    added the email feature and loading spinner

--- src/sections/ContactSection.jsx
+++ src/sections/ContactSection.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
+import emailjs from '@emailjs/browser';
 
 import SectionTitle from "../components/SectionTitle"
 import Wrapper from "../components/Wrapper"
@@ -6,14 +7,19 @@ import Wrapper from "../components/Wrapper"
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { FaFilePdf, FaSquareXTwitter, FaLocationDot } from "react-icons/fa6";
 import { SiGmail, SiLeetcode } from "react-icons/si";
+import Spinner from "../components/Spinner";
 
 
 const ContactSection = () => {
+    const form = useRef();
+
+    const [isError, setISError] = useState(" ");
+    const [isLoading, setIsLoading] = useState(false);
 
     const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        message: ''
+        user_name: '',
+        user_email: '',
+        user_message: ''
     });
 
     const handleChange = (e) => {
@@ -24,41 +30,64 @@ const ContactSection = () => {
         });
     };
 
-    const handleSubmit = (e) => {
+    const sendEmail = (e) => {
         e.preventDefault();
-        // You can add your form submission logic here
-        console.log(formData);
-        // Reset form after submission
-        setFormData({
-            name: '',
-            email: '',
-            message: ''
-        });
+
+        setIsLoading(true);
+        emailjs
+            .sendForm('service_1uhqref', 'template_hfxjk4i', form.current, {
+                publicKey: 'ah_t0Bdumhr9niCrl',
+            })
+            .then(
+                () => {
+                    setFormData({
+                        user_name: '',
+                        user_email: '',
+                        user_message: ''
+                    });
+                    setISError("no error");
+                    setIsLoading(false);
+
+                    setTimeout(() => {
+                        setISError(" ")
+                    }, 4000)
+
+                },
+                (error) => {
+                    console.log('FAILED...', error.text);
+                    setISError("error")
+
+                    setIsLoading(false);
+                    setTimeout(() => {
+                        setISError(" ")
+                    }, 4000)
+                },
+            );
     };
 
     return (
         <Wrapper className="bg-primary-color">
             <SectionTitle title="contact me" />
             <div className="w-full flex flex-col justify-between md:flex-row p-5 md:p-20 mt-5 gap-10 md:gap-40">
-                <form onSubmit={handleSubmit} className="flex-1">
+                <form ref={form} onSubmit={sendEmail} className="flex-1">
                     <div className="mb-4">
                         <input
                             type="text"
-                            id="name"
-                            name="name"
-                            value={formData.name}
+                            id="user_name"
+                            name="user_name"
+                            value={formData.user_name}
                             onChange={handleChange}
                             className="w-full border-gray-300 rounded-md focus:border-indigo-500 focus:ring focus:ring-indigo-200 outline-none border-none px-4 py-3 shadow-sm"
                             required
-                            placeholder="your name"
+                            placeholder="Your name"
                         />
                     </div>
                     <div className="mb-4">
                         <input
                             type="email"
-                            id="email"
-                            name="email"
-                            value={formData.email}
+                            id="user_email"
+                            name="user_email"
+                            value={formData.user_email}
                             onChange={handleChange}
                             className="w-full border-gray-300 rounded-md focus:border-indigo-500 focus:ring focus:ring-indigo-200 outline-none border-none px-4 py-3 shadow-sm"
                             required
@@ -67,21 +96,24 @@ const ContactSection = () => {
                     </div>
                     <div className="mb-6">
                         <textarea
-                            id="message"
-                            name="message"
-                            value={formData.message}
+                            id="user_message"
+                            name="user_message"
+                            value={formData.user_message}
                             onChange={handleChange}
                             rows="4"
                             className="w-full border-gray-300 rounded-md focus:border-indigo-500 focus:ring focus:ring-indigo-200 outline-none border-none px-4 py-2 shadow-sm"
                             required
-                            placeholder="you message here!"
+                            placeholder="Your message here!"
                         ></textarea>
                     </div>
                     <div className="text-center">
-                        <button className={`block w-fit border border-white hover:border-black bg-black hover:bg-white text-white hover:text-black transition-all duration-100 ease-in-out rounded-sm text-lg uppercase font-medium cursor-pointer px-3 py-2`}>send message</button>
+                        <button className={`block w-[160px] border border-white hover:border-black bg-black hover:bg-white text-white hover:text-black transition-all duration-100 ease-in-out rounded-sm text-lg uppercase font-medium cursor-pointer px-3 py-2`}>{isLoading ?
+                            <Spinner />
+                            : "Send Message"}</button>
                     </div>
-                    <p className="text-sm md:text-lg capitalize text-green-600 mt-5"> message sent successfully</p>
+                    <p className={`text-sm md:text-lg capitalize ${isError === " " ? "hidden" : "block"} mt-5`}>Message sent successfully</p>
                 </form>
+
                 <div className="flex-1">
                     <ul className="w-fit h-fit flex justify-center flex-col gap-4">
                         <li className="flex items-center gap-2 text-lg md:text-xl">
@@ -134,3 +166,4 @@ const ContactSection = () => {
 }
 
 export default ContactSection
+
